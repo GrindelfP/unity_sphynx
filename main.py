73c039b7e6a605b152a67c3e7c513a7e7797f0c3
GrindelfP <grindelf.perlomutrovij@gmail.com>
2022-01-27 18:58:57 +0300

    add time indication to main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from time import time
 from code_finding import code_checking
 from utility import receive_code, greetings
 
 greetings()
 users_code: str = receive_code()
-print("We are in! Your code was ", code_checking(users_code), "!", sep="")
+current_time: float = time()
+correct_code: str = code_checking(users_code)
+total_time: float = time() - current_time
+print("We are in! Your code was ", correct_code, "! \nWe've done the process in ", round(total_time, 8),
+      " seconds.", sep="")
