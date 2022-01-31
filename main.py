dd7cd6dd575402ee2bb7bdd24c6d5e85b7a24fe5
GrindelfP <grindelf.perlomutrovij@gmail.com>
2022-01-31 22:08:54 +0300

    add exit question to main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from code_finding import code_checking
 from utility import receive_code, greetings
 
 greetings()
-exit = True
-while exit:
+if_exit = True
+while if_exit:
     users_code: str = receive_code()
     current_time: float = time()
     correct_code: str = code_checking(users_code)
@@ -12,5 +12,7 @@ while exit:
     print("We are in! Your code was ", correct_code, "! \nWe've done the process in ", round(total_time, 8),
           " seconds.", sep="")
     exit_possibility = input("Do you want to continue? (Y/N) ")
-    if exit_possibility == "N":
-        exit = False
+    if exit_possibility.upper() == "N":
+        if_exit = False
+    elif exit_possibility.upper() != "Y":
+        print("I take that as an agreement!")
