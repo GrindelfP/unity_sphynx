0c00fb2fcf954835768c5dbd6ae1b621ae5bac37
Grindelf <63808275+GrindelfP@users.noreply.github.com>
2022-01-31 21:35:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from time import time
+import time
 from code_finding import code_checking
 from utility import receive_code, greetings
 
 greetings()
 users_code: str = receive_code()
-current_time: float = time()
+current_time: float = time.time()
 correct_code: str = code_checking(users_code)
-total_time: float = time() - current_time
+total_time: float = time.time() - current_time
 print("We are in! Your code was ", correct_code, "! \nWe've done the process in ", round(total_time, 8),
       " seconds.", sep="")
